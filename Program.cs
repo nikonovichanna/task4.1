09c55cbe7ad774ea0380c6533cd911db44e76f4b
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-09-03 19:07:46 +0300

    add task4.1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int ibase = Promt("Введите основание: ");
+int exponent = Promt("Введите показатель степени: ");
+System.Console.WriteLine($"Число {ibase} в степени {exponent} = {Power(ibase, exponent)}");
+
+int Promt (string message)
+{
+    System.Console.WriteLine(message);
+    string readInput = System.Console.ReadLine();
+    int result =int.Parse(readInput);
+    return result;
+}
+
+int Power(int ibase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= ibase;
+    }
+    return power;
+}
+
+
+
